3f3c1cba56c616001b0e1a58480ff07af9a57e58
Kaustubh <kaustubhr2001@gmail.com>
2024-03-10 00:11:39 +0530

    Update views and add employees form

--- officemanagement/employee/views.py
+++ officemanagement/employee/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from templates import  *
 # from models import *
 
@@ -9,17 +9,19 @@ def index(request):
 	return render(request, 'index.html')
 
 def add_employee(request):
-	# name  = request.POST["name"]
-	# last_name = request.POST["last_name"]
-	# email = request.POST["email"]
-	# phone = request.POST["phone"]
-	# address = request.POST["address"]
-	# salary = request.POST["salary"]
-	# position = request.POST["position"]
-	# department = request.POST["department"]
-	# hire_date = request.POST["hire_date"]
-	# employee = Employees(name=name, last_name=last_name, email=email, phone=phone, address=address, salary=salary, position=position, department=department, hire_date=hire_date)
-	# employee.save()
+	if request.method == 'POST':
+
+		name  = request.POST["name"]
+		last_name = request.POST["last_name"]
+		email = request.POST["email"]
+		phone = request.POST["phone"]
+		address = request.POST["address"]
+		salary = request.POST["salary"]
+		position = request.POST["position"]
+		department = request.POST["department"]
+		hire_date = request.POST["hire_date"]
+		employee = Employees(name=name, last_name=last_name, email=email, phone=phone, address=address, salary=salary, position=position, department=department, hire_date=hire_date)
+		employee.save()
 	return render(request, 'add_employee.html')
 
 def remove_employee(request):
