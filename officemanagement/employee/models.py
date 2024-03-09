f9c6ef65bee2623a037cb108b2ac42536ae1f615
Kaustubh <kaustubhr2001@gmail.com>
2024-03-09 23:10:57 +0530

    Schemas created

--- officemanagement/employee/models.py
+++ officemanagement/employee/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Departments(models.Model):
+	name = models.CharField(max_length=100)
+	location = models.CharField(max_length=100)
+
+
+class Role(models.Model):
+	name = models.CharField(max_length=100)
+	description = models.CharField(max_length=100)
+
+
+class Employees(models.Model):
+	name = models.CharField(max_length=100)
+	last_name = models.CharField(max_length=100)
+	age = models.IntegerField()
+	salary = models.IntegerField()
+	department = models.ForeignKey(Departments, on_delete=models.CASCADE)
+	position = models.ForeignKey(Role, on_delete=models.CASCADE)
+	phone = models.CharField(max_length=100)
+	hire_date = models.DateField()
